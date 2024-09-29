c6059bb73ffd515c4302495d164c4be67363775a
LeeJeongGi <apple4rhk@naver.com>
2024-09-29 20:31:09 +0900

    first commit

---
+++ src/main/kotlin/com/_week/hhplus_tdd_practice/HhplusTddPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com._week.hhplus_tdd_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusTddPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HhplusTddPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/com/_week/hhplus_tdd_practice/HhplusTddPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com._week.hhplus_tdd_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HhplusTddPracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
