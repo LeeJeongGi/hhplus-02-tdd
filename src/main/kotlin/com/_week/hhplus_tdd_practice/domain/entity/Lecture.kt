b51fac6c62c9e0292aacf80781884f0afb544138
LeeJeongGi <apple4rhk@naver.com>
2024-09-30 23:30:20 +0900

    feat : 특강 정보, 특강 신청 히스토리 저장할 Entity 생성

---
+++ src/main/kotlin/com/_week/hhplus_tdd_practice/domain/entity/Lecture.kt
@@ -0,0 +1,34 @@
+package com._week.hhplus_tdd_practice.domain.entity
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "lecture")
+data class Lecture(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    val title: LectureType,
+
+    @Column(nullable = false)
+    val presenter: String,
+
+    @Column(nullable = false)
+    val capacity: Int = 30,
+
+    @Column(nullable = false)
+    val date: LocalDateTime,
+
+    @OneToMany(mappedBy = "lecture", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    val registrationHistory: List<LectureEnrollmentHistory> = mutableListOf(),
+)
+
+enum class LectureType(val displayName: String) {
+    Ai("Machine Learning"),
+    BACK_END("Back End"),
+    FRONT_END("Front End"),;
+}
