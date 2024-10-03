d0642b35fee6020d67e295ce3bda88d74613a192
LeeJeongGi <apple4rhk@naver.com>
2024-10-03 21:17:56 +0900

    feat : 동시성 처리를 위해 특강별 실시간 신청현황 수를 관리하는 테이블 생성

---
+++ src/main/kotlin/com/_week/hhplus_tdd_practice/infra/LectureApplierRepository.kt
@@ -0,0 +1,17 @@
+package com._week.hhplus_tdd_practice.infra
+
+import com._week.hhplus_tdd_practice.infra.entity.LectureApplier
+import jakarta.persistence.LockModeType
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface LectureApplierRepository : JpaRepository<LectureApplier, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT la FROM LectureApplier la WHERE la.lectureId = :lectureId")
+    fun findByLectureIdWithLock(@Param("lectureId") lectureId: Long): LectureApplier?
+}
